3a3dceb0a098127463ccbea829f06602ba149890
Johnny <johnny@classbundl.com>
2018-04-02 18:29:54 -0500

    Add nav bar to main view

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,12 +9,34 @@
 
         <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}">
         <script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
+
+        @yield('head')
     </head>
     <body>
         <div class="flex-center position-ref full-height">
+            <nav class="navbar is-secondary">
+                <div class="navbar-brand">
+                    <span class="navbar-item" href="https://bulma.io">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuz3LXMcTJCWPKG_TBAt6F9mEfUnik7s7ReD2iMtF_mOJvy9X1" alt="Forza">
+                    </span>
+                    <div class="navbar-burger burger" data-target="navMenuColordark-example">
+                        <span></span>
+                        <span></span>
+                    </div>
+                </div>
+
+                <div id="navMenuColordark-example" class="navbar-menu">
+                    <div class="navbar-start">
+                        <a class="navbar-item{{ Route::currentRouteName() === 'auctions.index' ? ' is-active' : '' }}" href="{{ route('auctions.index') }}">Auctions</a>
+                        <a class="navbar-item{{ Route::currentRouteName() === 'cars.index' ? ' is-active' : '' }}" href="{{ route('cars.index') }}">Cars</a>
+                    </div>
+                </div>
+            </nav>
             <div class="content">
                 @yield('content')
             </div>
         </div>
+
+        @yield('scripts')
     </body>
 </html>
