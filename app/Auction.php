2702c46c1c3387b9886363483561fdd583980c87
Johnny <johnny@classbundl.com>
2018-04-02 18:19:26 -0500

    Add Car relationship to Auction model

--- app/Auction.php
+++ app/Auction.php
@@ -3,8 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Auction extends Model
 {
     protected $fillable = ['car_id', 'buy_price', 'sell_price'];
+
+    public function car(): BelongsTo
+    {
+        return $this->belongsTo(Car::class);
+    }
 }
