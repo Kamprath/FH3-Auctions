a3a62b5ea1ba1d28da94e074c4cef88f845dd935
Johnny <johnny@classbundl.com>
2018-04-02 18:33:26 -0500

    Add statistics section to Car view

--- resources/views/cars/show.blade.php
+++ resources/views/cars/show.blade.php
@@ -7,6 +7,7 @@
         <div class="hero-body">
             <div class="container">
                 <p class="title">
+                    <i class="fa fa-car"></i>&nbsp;
                     {{ $car->year }} {{ $car->make }} {{ $car->model }}
                     <button class="button is-primary" style="float: right;"><i class="fa fa-plus"></i>&nbsp;&nbsp;New Auction</button>
                 </p>
@@ -15,13 +16,45 @@
     </section>
 
     <section class="section">
+        <nav class="level">
+            <div class="level-item has-text-centered">
+                <div>
+                    <p class="heading">Avg. Buy Price</p>
+                    <p class="title">$20,000</p>
+                </div>
+            </div>
+            <div class="level-item has-text-centered">
+                <div>
+                    <p class="heading">Avg. Sell Price</p>
+                    <p class="title">$76,000</p>
+                </div>
+            </div>
+            <div class="level-item has-text-centered">
+                <div>
+                    <p class="heading">ROI</p>
+                    <p class="title">6%</p>
+                </div>
+            </div>
+            <div class="level-item has-text-centered">
+                <div>
+                    <p class="heading">Total Profit</p>
+                    <p class="title">$213,500</p>
+                </div>
+            </div>
+        </nav>
+    </section>
+
+    <section class="section">
+        <h1 class="title">Auctions</h1>
+
         @if (count($car->auctions) > 0)
             <table class="table">
                 <thead>
                     <tr>
-                        <th>Type</th>
+                        <th>Transaction</th>
                         <th>Price</th>
                         <th>Date</th>
+                        <th></th>
                     </tr>
                 </thead>
                 <tbody>
@@ -32,6 +65,11 @@
                                 {!! $auction->sell_price > 0 ? '&nbsp;' : '-'!!}${{ number_format($auction->buy_price > 0 ? $auction->buy_price : $auction->sell_price) }}
                             </td>
                             <td>{{ $auction->created_at->format('F jS g:iA') }}</td>
+                            <td>
+                                <a class="button is-secondary is-small" href="{{ route('auctions.edit', ['id' => $auction->id]) }}">
+                                    <i class="fa fa-edit"></i> Edit
+                                </a>
+                            </td>
                         </tr>
                     @endforeach
                 </tbody>
