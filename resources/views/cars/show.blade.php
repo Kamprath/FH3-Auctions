ed8e68a4bf16295652243253449305b7f1250630
Johnny <johnny@classbundl.com>
2018-04-03 10:45:28 -0500

    Update auctions table on Car view so it fits within a container

--- resources/views/cars/show.blade.php
+++ resources/views/cars/show.blade.php
@@ -45,37 +45,39 @@
     </section>
 
     <section class="section">
-        <h1 class="title">Auctions</h1>
+        <div class="container">
+            <h1 class="title">Auctions</h1>
 
-        @if (count($car->auctions) > 0)
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th>Transaction</th>
-                        <th>Price</th>
-                        <th>Date</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($car->auctions as $auction)
+            @if (count($car->auctions) > 0)
+                <table class="table">
+                    <thead>
                         <tr>
-                            <td>{{ $auction->buy_price > 0 ? 'Purchase' : 'Sale' }}</td>
-                            <td>
-                                {!! $auction->sell_price > 0 ? '&nbsp;' : '-'!!}${{ number_format($auction->buy_price > 0 ? $auction->buy_price : $auction->sell_price) }}
-                            </td>
-                            <td>{{ $auction->created_at->format('F jS g:iA') }}</td>
-                            <td>
-                                <a class="button is-secondary is-small" href="{{ route('auctions.edit', ['id' => $auction->id]) }}">
-                                    <i class="fa fa-edit"></i> Edit
-                                </a>
-                            </td>
+                            <th>Transaction</th>
+                            <th>Price</th>
+                            <th>Date</th>
+                            <th></th>
                         </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        @else
-            <p class="has-text-centered">There are no auctions for this car.</p>
-        @endif
+                    </thead>
+                    <tbody>
+                        @foreach ($car->auctions as $auction)
+                            <tr>
+                                <td>{{ $auction->buy_price > 0 ? 'Purchase' : 'Sale' }}</td>
+                                <td>
+                                    {!! $auction->sell_price > 0 ? '&nbsp;' : '-'!!}${{ number_format($auction->buy_price > 0 ? $auction->buy_price : $auction->sell_price) }}
+                                </td>
+                                <td>{{ $auction->created_at->format('F jS g:iA') }}</td>
+                                <td>
+                                    <a class="button is-secondary is-small" href="{{ route('auctions.edit', ['id' => $auction->id]) }}">
+                                        <i class="fa fa-edit"></i> Edit
+                                    </a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            @else
+                <p class="has-text-centered">There are no auctions for this car.</p>
+            @endif
+        </div>
     </section>
 @endsection
